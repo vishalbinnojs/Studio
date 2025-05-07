e7e4523de34dd3fb9c291046a209e1a2e0ccc7f8
Vishal Bhardwaj <vishalbinnojs@gmail.com>
2025-05-07 13:05:48 +0530

    first commit

---
+++ Root/studio.js
@@ -0,0 +1,8 @@
+// const hamburger = document.querySelector('.hamburger');
+// const links = document.querySelector('.links');
+
+// if (hamburger && links) {
+//   hamburger.addEventListener('click', () => {
+//     links.classList.toggle('active');
+//   });
+// }
